3be8f1f60aa7028a827c3623f0acce57ceadf4b1
viniciusaraujoramos <vinicius.ara.ramos@gmail.com>
2026-05-20 17:01:02 -0300

    ✨ feat: Adicionando uma function e um button em cada exercicio para melhor visualização

---
+++ Aula_05/ex.js
@@ -0,0 +1,118 @@
+function ex1(){
+    console.log("=====EXERCÍCIO 1=====")
+
+    for(let i=1; i<11; i++){
+        console.log(i)
+    }
+}
+
+function ex2(){
+    console.log("=====EXERCÍCIO 2=====")
+
+    let tabuada = Number(prompt("Digite um número para que eu possa gerar a tabuada:"))
+
+    for (let tab = 1; tab<=10; tab++){
+        let tabu = tabuada*tab
+        console.log(tabu)
+        
+    }
+}
+
+function ex3(){
+    console.log("=====EXERCÍCIO 3=====")
+
+    let N = Number(prompt("Digite um número:"))
+    let final = 0
+    for(let i =1; i<=N; i++){
+        final = final + i
+        console.log(final);
+        
+    }
+}
+
+function ex4(){
+    
+}
+function ex5(){
+    
+}
+function ex6(){
+    
+}
+function ex7(){
+    
+}
+function ex8(){
+    
+}
+function ex9(){
+    
+}
+function ex10(){
+    
+}
+function ex11(){
+    
+}
+function ex12(){
+    
+}
+function ex13(){
+    
+}
+function ex14(){
+    
+}
+function ex15(){
+    
+}
+
+
+
+
+
+// console.log("=====EXERCÍCIO 4=====")
+
+
+
+// console.log("=====EXERCÍCIO 5=====")
+
+
+
+// console.log("=====EXERCÍCIO 6=====")
+
+
+
+// console.log("=====EXERCÍCIO 7=====")
+
+
+
+// console.log("=====EXERCÍCIO 8=====")
+
+
+
+// console.log("=====EXERCÍCIO 9=====")
+
+
+
+// console.log("=====EXERCÍCIO 10=====")
+
+
+
+// console.log("=====EXERCÍCIO 11=====")
+
+
+
+// console.log("=====EXERCÍCIO 12=====")
+
+
+
+// console.log("=====EXERCÍCIO 13=====")
+
+
+
+// console.log("=====EXERCÍCIO 14=====")
+
+
+
+// console.log("=====EXERCÍCIO 15=====")

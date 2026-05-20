8d842a730663f3e62c0ace43c127979bfb7ebf61
viniciusaraujoramos <vinicius.ara.ramos@gmail.com>
2026-05-20 17:25:40 -0300

    ✨ feat: Metade dos exercícios

--- Aula_05/ex.js
+++ Aula_05/ex.js
@@ -31,10 +31,26 @@ function ex3(){
 }
 
 function ex4(){
-    
+    let par = 0
+    let comp
+
+    for(let i = 1; i <= 50; i++){
+        par++
+        comp = par%2
+        if(comp == 0){
+            console.log(par)
+        }
+    }
 }
 function ex5(){
-    
+    const randon = Math.random() * 100;
+    let resposta = Number(prompt("Digite um numero de 1 a 100"));
+    while(resposta != randon){
+        
+        resposta = Number(prompt("Digite outro número"));
+    }
+    console.log("Você acertou, o número era: " + randon)
+
 }
 function ex6(){
     
